38abf78e2e839c34a62d53f89f7c4ed225429b5e
stoneloong <stoneloongc@gmail.com>
2025-10-28 23:42:53 +0800

    eight-week-two-code

--- learn55.c
+++ learn55.c
@@ -1,34 +1,30 @@
-#include <stdio.h>
-
-int main(void)
+#include<stdio.h>
+int main()
 {
-    int a[50];          /* 存输入的数据 */
-    int i, j;           /* 循环变量 */
-    int sum = 0;        /* 和 */
-    int s   = 0;        /* 计数器：>= 平均值的个数 */
-    double ave = 0.0;   /* 平均值，用浮点避免整除丢失小数 */
-
-    /* 读入数据，-1 结束 */
-    for (i = 0; i < 50; ++i)
+    int a[50],i,b,sum=0,s=0;
+    float ave;
+    printf("请输入整数");
+    for(i=0;i<50;i++)
     {
-        scanf("%d", &a[i]);
-        if (a[i] == -1)      /* 遇到结束标志 */
-            break;
+        scanf("%d",&b);
+       
+        if(b==-1)
+        break;
+         a[i]=b;
     }
-
-    /* 计算和 */
-    for (j = 0; j < i; ++j)
-        sum += a[j];
-
-    /* 计算平均值 */
-    if (i > 0)
-        ave = (double)sum / i;
-
-    /* 统计 >= 平均值的个数 */
-    for (j = 0; j < i; ++j)
-        if (a[j] >= ave)
-            ++s;
-
-    printf("s=%d\n", s);
+    if(i==0)
+    return 0;
+    for(int j=0;j<i;j++)
+    {
+        sum+=a[j];
+    }
+    ave=sum/(float)i;
+    for(int j=0;j<i;j++)
+    {
+        if(a[j]>ave)
+        s++;
+        
+    }
+    printf("%d",s);
     return 0;
 }
